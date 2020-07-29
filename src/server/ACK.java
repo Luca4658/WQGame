9ecff56d6bc65eece7455937f39b43dbed0cd9b6
luca <lccanessa@gmail.com>
2020-07-29 17:39:36 +0200

    Acknowledge messages enumeration

---
+++ src/server/ACK.java
@@ -0,0 +1,19 @@
+package server;
+
+
+public enum ACK
+	{
+		/*
+		 * USERS
+		 */
+		AlreadyRegistered,
+		EmptyField,
+		UserRegistered,
+		RegistrationError,
+		UserFound,
+		UserNotFound,
+		UserDeleted,
+		UserNotDeleted,
+		
+		OK;
+	}
