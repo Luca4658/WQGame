6bfb250de580c812537a2a8cef6abebbe676dd45
luca <lccanessa@gmail.com>
2020-08-01 17:24:28 +0200

    server testing

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,5 +1,10 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -7,6 +12,23 @@ import java.rmi.registry.Registry;
 
 import server.RegRMInterface;
 import server.User;
+
+
+enum ClientMSG
+	{
+		LOGIN,
+		LOGOUT,
+		ADDFRIEND,
+		GETFRIENDS,
+		GETNFRIENDS,
+		SENDCH,
+		CHSTARTED,
+		UPDATEINFO,
+		GETPOINTS,
+		GETRANK;
+	}
+
+
 
 public class Main
 	{
@@ -24,5 +46,54 @@ public class Main
 					{
 						e.printStackTrace();
 					}
+
+				DataOutputStream mout = null;
+				BufferedReader min = null;
+
+				try
+					{
+						Socket me = new Socket( "localhost", 46058 );
+						mout = new DataOutputStream( me.getOutputStream( ) );
+						min = new BufferedReader( new InputStreamReader( me.getInputStream( ) ) );
+					}
+				catch( IOException e )
+					{
+						e.printStackTrace( );
+					}
+
+
+				String msg = ClientMSG.LOGIN.name() + "\n";
+				try
+					{
+						mout.writeBytes( msg );
+						mout.writeBytes( "Luca" + "\n" );
+						mout.writeBytes( "TuaSorella\n" );
+
+						System.out.println( min.readLine( ) );
+
+						mout.writeBytes( ClientMSG.GETNFRIENDS.name() + "\n" );
+
+						System.out.println( min.readLine( ) );
+
+
+						mout.writeBytes( ClientMSG.GETFRIENDS.name() + "\n" );
+
+						System.out.println( min.readLine( ) );
+
+						mout.writeBytes( ClientMSG.UPDATEINFO.name() + "\n" );
+						mout.writeBytes(  "Luca" + "\n" );
+						mout.writeBytes(  "Cicco" + "\n" );
+						mout.writeBytes(  "TuaSorella" + "\n" );
+
+						System.out.println( min.readLine( ) );
+
+						Thread.sleep( 1000 );
+					}
+				catch( IOException | InterruptedException e )
+					{
+						e.printStackTrace( );
+					}
+
+
 			}
 	}
