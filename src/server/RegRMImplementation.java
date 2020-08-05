ef89704dae596194b7d000f1225adec86e1d471d
luca <lccanessa@gmail.com>
2020-08-05 20:44:38 +0200

    commented code

--- src/server/RegRMImplementation.java
+++ src/server/RegRMImplementation.java
@@ -1,22 +1,54 @@
+/************************************************
+ *                                              *
+ *            REGRMIMPLEMENTATION              *
+ *                                              *
+ ************************************************
+ *
+ */
 package server;
 
 import java.rmi.RemoteException;
 import java.rmi.server.*;
 
 
+
+/**
+ * In questa classe viene implementata e gestita la Remote Method Invocation
+ * usata dai client per effettuare la registrazione dell'User.
+ * Utilizza il metodo inserUser della classe Users per inserire l'utente
+ *
+ * @class   RegRMImplementation
+ * @author  Luca Canessa (Mat. 516639)
+ * @version 1.1
+ * @since   1.0
+ */
 public class RegRMImplementation extends RemoteServer implements RegRMInterface 
 	{
-		private static final long serialVersionUID = 1L;
-		private Users __udb;
-		private Friendships __fdb;
-		
+		private static final long serialVersionUID = 1L; ///< variabile necessaria per la serializzazione dell'oggetto
+		private Users       __udb; ///< database degli utenti
+		private Friendships __fdb; ///< database delle amicizie
+
+		/**
+		 * Costruttore della RMI necessaria per permettere la registrazione di un utente
+		 *
+		 * @param usrs  database degli User
+		 * @param frd    database delle Friendships
+		 */
 		public RegRMImplementation( Users usrs, Friendships frd )
 			{
 				__udb = usrs;
 				__fdb = frd;
 			}
-		
-		
+
+		/**
+		 * Implementazione del metodo usato per registrare l'User.
+		 * Tale metodo si basa sul metodo all'interno della classe Users
+		 * predisposto per questo utilizzo. Ritorna un valore di ACK.
+		 *
+		 * @param u User da inserire del database
+		 * @return //TODO
+		 * @throws RemoteException
+		 */
 		@Override
 		public ACK RegUser( User u ) throws RemoteException
 			{
--- src/server/RegRMInterface.java
+++ src/server/RegRMInterface.java
@@ -1,9 +1,36 @@
+/************************************************
+ *                                              *
+ *               REGRMINTERFACE                 *
+ *                                              *
+ ************************************************
+ *
+ */
 package server;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+
+
+
+/**
+ * In questa interfaccia viene usata per poter usare la RMI per registrare gli
+ * User.
+ *
+ * @class   RegRMInterface
+ * @author  Luca Canessa (Mat. 516639)
+ * @version 1.1
+ * @since   1.0
+ */
 public interface RegRMInterface extends Remote
 	{
+
+		/**
+		 * Metodo dichiarato per registrare l'utente. Ritorna un valore di ACK
+		 *
+		 * @param u User da registrare
+		 * @return risultato dell'operazione di registrazione
+		 * @throws RemoteException
+		 */
 		public ACK RegUser( User u ) throws RemoteException;
 	}
