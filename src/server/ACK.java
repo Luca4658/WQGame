7f41d6e32b99dd61f1083d50fb6f52839be2501c
luca <lccanessa@gmail.com>
2020-08-05 20:38:44 +0200

    added some messages & commented code

--- src/server/ACK.java
+++ src/server/ACK.java
@@ -1,41 +1,57 @@
+/************************************************
+ *                                              *
+ *                    ACK                       *
+ *                                              *
+ ************************************************
+ *
+ */
 package server;
 
-
+/**
+ * In questo file vengono implementati i messaggi da inviare al client a
+ * termine delle operazioni richieste
+ *
+ * @class   ACK
+ * @author  Luca Canessa (Mat. 516639)
+ * @version 1.6
+ * @since   1.0
+ */
 public enum ACK
 	{
 		//	USER	//
-		PasswordUnmatch,
-		UserAlreadyLoggedIn,
-		LoggedIn,
-		LoggedOut,
-		ONLINE,
-		OFFLINE,
-		INCHALLENGE,
+		PasswordUnmatch, ///< password non corrisponde a quella del User
+		UserAlreadyLoggedIn, ///< User già entrato
+		NotLogged, ///<< User non loggato
+		LoggedIn, ///<User loggato
+		LoggedOut, ///<User uscito
+		ONLINE, ///<stato dell'User online
+		OFFLINE, ///<stato dell'User  offline
+		INCHALLENGE, ///<stato dell'User in sfida
 
 		//	USERS	//
-		AlreadyRegistered,
-		EmptyField,
-		UserRegistered,
-		RegistrationError,
-		UserFound,
-		UserNotFound,
-		UserDeleted,
-		UserNotDeleted,
+		AlreadyRegistered, ///<User già registrato
+		EmptyField, ///<campi utente vuoti
+		UserRegistered, ///<User registrato
+		RegistrationError, ///<problema generale durante la registrazione dell'User
+		UserFound, ///<User trovato nel database
+		UserNotFound, ///<User non trovato nel database
+		UserDeleted, ///<User deregistrato
+		UserNotDeleted, ///<User non deregistrato
 		
 		//	FRIENDS	//
-		AlreadyFriends,
-		FriendNotFound,
-		FriendAdded,
-		UserAdded,
-		AlreadyExisting,
-		FriendRemoved,
+		AlreadyFriends, ///<User(s) già amici
+		FriendNotFound, ///<User amico non trovato
+		FriendAdded, ///<User registrato come amico
+		UserAdded, ///<User inserito nel database
+		AlreadyExisting, ///<
+		FriendRemoved, ///<User amico rimosso dalla lista degli amici
 
 		//  CHALLENGE //
-		Accepted,
-		Rejected,
+		Accepted, ///<sfida accettata
+		Rejected, ///<sfida rifiutata
 
-
-		OperationUnknown,
-		OK,
-		ERROR;
+		// GENERAL  //
+		OperationUnknown, ///<operazione richiesta non riconosciuta
+		OK, ///<azione eseguita
+		ERROR; ///<errore generale durante l'operazione
 	}
