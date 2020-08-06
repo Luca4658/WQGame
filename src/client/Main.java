9e4aa9e1886707ab1b27b54e5686d82a8c00ba7e
luca <lccanessa@gmail.com>
2020-08-06 20:05:22 +0200

    testing server tasks

--- src/client/Main.java
+++ src/client/Main.java
@@ -17,42 +17,46 @@ import server.User;
 
 enum ClientMSG
 	{
-		LOGIN,
-		LOGOUT,
-		ADDFRIEND,
-		GETFRIENDS,
-		GETNFRIENDS,
-		SENDCH,
-		ACCEPTEDCH,
-		UPDATEINFO,
-		GETPOINTS,
-		GETRANK;
+		LOGIN, ///< richiesta di login
+		LOGOUT, ///< richiesta di logout
+		ADDFRIEND, ///< richiesta di aggiunta di amico alla lista
+		GETFRIENDS, ///< richiesta lista amicizie
+		GETNFRIENDS, ///< richiesto numero di amici
+		STARTCH, ///< richiesta di inizio di una sfida
+		ACCEPTEDCH, ///< accettazione della sfida arrivata
+		UPDATEINFO, ///< richiesta di aggiornamento dei dati
+		GETPOINTS, ///< richiesta punti totalizzati
+		GETRANK,  ///< richiesta della classifica
+		RMUSER, ///< richiesta rimozione dell'User
+		RMFRIEND; ///< richiesta rimozione dell'amico
 	}
+
 
 
 
 public class Main
 	{
-		public static void main( String[] args )
+		public static void main( String[] args ) throws IOException
 			{
-
-				Scanner usrname = new Scanner( System.in );
-				User u = new User( "Luca3", "TuaSorella" );
+				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				System.out.println( "Enter nickname" );
+				String nick = new String( br.readLine( ) );
+				User u = new User( nick, "Password" );
 				
-				try
-					{
-						Registry reg = LocateRegistry.getRegistry( 21895 );
-						RegRMInterface stub = (RegRMInterface) reg.lookup( "ServerRMI" );
-						System.err.println( stub.RegUser( u ) );
-					}
-				catch( RemoteException | NotBoundException e )
-					{
-						e.printStackTrace();
-					}
+//				try
+//					{
+//						Registry reg = LocateRegistry.getRegistry( 21895 );
+//						RegRMInterface stub = (RegRMInterface) reg.lookup( "ServerRMI" );
+//						System.err.println( stub.RegUser( u ) );
+//					}
+//				catch( RemoteException | NotBoundException e )
+//					{
+//						e.printStackTrace();
+//					}
 
 				DataOutputStream mout = null;
 				BufferedReader min = null;
-//
+
 				try
 					{
 						Socket me = new Socket( "localhost", 46058 );
@@ -63,44 +67,57 @@ public class Main
 					{
 						e.printStackTrace( );
 					}
-//
 
 
-//
-//				String msg = ClientMSG.ACCEPTEDCH.name() + "\n";
-//				try
-//					{
-//						mout.writeBytes( msg );
-//						mout.writeBytes( "Luca1" + "\n" );
-//						mout.writeBytes( "Password\n" );
-//
-//						System.out.println( min.readLine( ) );
-//
-//						mout.writeBytes( ClientMSG.GETNFRIENDS.name() + "\n" );
-//
-//						System.out.println( min.readLine( ) );
-//
-//
-//						mout.writeBytes( ClientMSG.GETFRIENDS.name() + "\n" );
-//
-//						System.out.println( min.readLine( ) );
-//
-//						mout.writeBytes( ClientMSG.UPDATEINFO.name() + "\n" );
-//						mout.writeBytes(  "Luca" + "\n" );
-//						mout.writeBytes(  "Cicco" + "\n" );
-//						mout.writeBytes(  "TuaSorella" + "\n" );
-//
-//						System.out.println( min.readLine( ) );
-//
-//						mout.writeBytes( ClientMSG.GETRANK.name( ) + "\n" );
-//						System.out.println( min.readLine() );
-//
-//						Thread.sleep( 1000 );
-//					}
-//				catch( IOException /*| InterruptedException*/ e )
-//					{
-//						e.printStackTrace( );
-//					}
+				try
+					{
+						mout.writeBytes( ClientMSG.LOGIN.name() + "\n");
+						mout.writeBytes( u.getID( ) + "\n");
+						mout.writeBytes( "Password" + "\n" );
+
+
+						System.out.println( min.readLine( ) );
+
+						mout.writeBytes( ClientMSG.GETFRIENDS.name( ) + "\n" );
+						System.out.println( min.readLine( ) );
+
+						mout.writeBytes( ClientMSG.GETNFRIENDS.name( ) + "\n" );
+						System.out.println( min.readLine( ) );
+
+//						Thread.sleep( 10000 );
+
+						mout.writeBytes( ClientMSG.STARTCH.name( ) + "\n" );
+						System.out.println( "Write Friend" );
+						String frd = br.readLine( );
+						System.out.println( frd );
+						mout.writeBytes( frd + "\n" );
+						System.out.println( min.readLine( ) );
+
+
+
+
+
+						mout.writeBytes( ClientMSG.GETPOINTS.name( ) + "\n" );
+						System.out.println( min.readLine( ) );
+
+						mout.writeBytes( ClientMSG.GETRANK.name( ) + "\n" );
+						System.out.println( min.readLine( ) );
+
+						mout.writeBytes( "ILBUDELLODITUMA" + "\n" );
+						System.out.println( min.readLine( ) );
+
+
+
+						mout.writeBytes( ClientMSG.LOGOUT.name( ) + "\n" );
+						System.out.println( min.readLine( ) );
+
+						Thread.sleep( 100 );
+
+					}
+				catch( IOException | InterruptedException /*| InterruptedException*/ e )
+					{
+						e.printStackTrace( );
+					}
 
 
 			}
